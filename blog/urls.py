0ea85c4c76b6fee0e63eb74fa69e63c9f676c220
syinblack <syinblack@naver.com>
2021-09-02 18:13:13 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),     # blog/숫자 형태일때는 views.single_post_page함수로 전달
+    path('<int:pk>/', views.PostDetail.as_view()),     # blog/숫자 형태일때는 views.single_post_page함수로 전달
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,7 +7,8 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
-
+class PostDetail(DetailView):
+    model = Post
 
 # FBV방식 (이제 사용안함.)
 """def index(request):
@@ -22,14 +23,15 @@ class PostList(ListView):
         }
     )
 
-"""
+
 def single_post_page(request, pk):      # pk는 데이터베이스에서 데이터를 불러오기 위함.
     post = Post.objects.get(pk=pk)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post' : post,
         }
     )
+"""
