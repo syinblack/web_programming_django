becdaa810f646221029a1d0425265ed6c69f5d82
syinblack <syinblack@naver.com>
2021-09-06 01:19:25 +0900

    태그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view()),     # blog/숫자 형태일때는 views.single_post_page함수로 전달
+    path('<int:pk>/', views.PostDetail.as_view()),     # blog/숫자 형태일때는 views.single_post_page 함수로 전달
 ]
