4f315d755c412ed184f2e0e53f0f763f077bb351
syinblack <syinblack@naver.com>
2021-09-05 16:45:15 +0900

    삭제된 작성자는 None으로 표시하기

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # CASCADE
 
     # 게시물 제목 나타내기, [pk] title
     def __str__(self):
