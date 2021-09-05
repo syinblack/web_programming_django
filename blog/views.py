407a15669f30235d07ee26653d5dc71655fd3e29
syinblack <syinblack@naver.com>
2021-09-05 20:00:09 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,12 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 
+    def get_context_data(self, **Kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
+
 # FBV방식 (이제 사용안함.)
 """def index(request):
     posts = Post.objects.all().order_by('-pk')      # 모든 post 레코드를 가져옴.
