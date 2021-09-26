ac245a0277bcba6eb226ae72997809dab8615cba
syinblack <syinblack@naver.com>
2021-09-26 20:56:55 +0900

    도커에 장고만 담기(수정)

--- blog/views.py
+++ blog/views.py
@@ -104,7 +104,7 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'        # CreateView, UpdateView는 모델명_form.html이 default template
 
